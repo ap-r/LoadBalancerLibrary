cfcb0e55f65961710b299e8fc6891815ae42200c
AngelikaReszczyk <angelika.reszczyk@innovationsoftware.pl>
2024-07-23 20:01:04 +0200

    Add UnknownLoadBalancingAlgorithm constructor with default message

--- src/Exception/UnknownLoadBalancingAlgorithm.php
+++ src/Exception/UnknownLoadBalancingAlgorithm.php
@@ -4,4 +4,8 @@ namespace App\Exception;
 
 class UnknownLoadBalancingAlgorithm extends \InvalidArgumentException
 {
+    public function __construct(string $message = 'Unknown load balancing algorithm', int $code = 0, ?\Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
