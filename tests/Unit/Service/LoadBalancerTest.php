fd67594943fef80d413345e5c9a02cec2294529e
AngelikaReszczyk <angelika.reszczyk@innovationsoftware.pl>
2024-07-23 18:22:02 +0200

    Refactor tests and add more assertions (extended scenarios)

--- tests/Unit/Service/LoadBalancerTest.php
+++ tests/Unit/Service/LoadBalancerTest.php
@@ -27,23 +27,57 @@ class LoadBalancerTest extends TestCase
         $request = new Request(BigDecimal::of(0.1));
         $loadBalancer = new LoadBalancer($hosts, LoadBalancer::ROUND_ROBIN);
 
-        // First request should go to the first host
-        $loadBalancer->handleRequest($request);
-        $this->assertEquals(BigDecimal::of(0.3), $hosts[0]->getLoad());
-        $this->assertEquals(BigDecimal::of(0.6), $hosts[1]->getLoad());
-        $this->assertEquals(BigDecimal::of(0.1), $hosts[2]->getLoad());
+        $expectedLoads = [
+            // Request 1: Sent to Host 1
+            1 => [
+                BigDecimal::of('0.3'), // Load for Host 1
+                BigDecimal::of('0.6'), // Load for Host 2
+                BigDecimal::of('0.1')  // Load for Host 3
+            ],
+            // Request 2: Sent to Host 2
+            2 => [
+                BigDecimal::of('0.3'), // Load for Host 1
+                BigDecimal::of('0.7'), // Load for Host 2
+                BigDecimal::of('0.1')  // Load for Host 3
+            ],
+            // Request 3: Sent to Host 3
+            3 => [
+                BigDecimal::of('0.3'), // Load for Host 1
+                BigDecimal::of('0.7'), // Load for Host 2
+                BigDecimal::of('0.2')  // Load for Host 3
+            ],
+            // Request 4: Sent to Host 1
+            4 => [
+                BigDecimal::of('0.4'), // Load for Host 1
+                BigDecimal::of('0.7'), // Load for Host 2
+                BigDecimal::of('0.2')  // Load for Host 3
+            ],
+            // Request 5: Sent to Host 2
+            5 => [
+                BigDecimal::of('0.4'), // Load for Host 1
+                BigDecimal::of('0.8'), // Load for Host 2
+                BigDecimal::of('0.2')  // Load for Host 3
+            ],
+            // Request 6: Sent to Host 3
+            6 => [
+                BigDecimal::of('0.4'), // Load for Host 1
+                BigDecimal::of('0.8'), // Load for Host 2
+                BigDecimal::of('0.3')  // Load for Host 3
+            ],
+            // Request 7: Sent to Host 1
+            7 => [
+                BigDecimal::of('0.5'), // Load for Host 1
+                BigDecimal::of('0.8'), // Load for Host 2
+                BigDecimal::of('0.3')  // Load for Host 3
+            ]
+        ];
 
-        // Second request should go to the second host
-        $loadBalancer->handleRequest($request);
-        $this->assertEquals(BigDecimal::of(0.3), $hosts[0]->getLoad());
-        $this->assertEquals(BigDecimal::of(0.7), $hosts[1]->getLoad());
-        $this->assertEquals(BigDecimal::of(0.1), $hosts[2]->getLoad());
-
-        // Third request should go to the third host
-        $loadBalancer->handleRequest($request);
-        $this->assertEquals(BigDecimal::of(0.3), $hosts[0]->getLoad());
-        $this->assertEquals(BigDecimal::of(0.7), $hosts[1]->getLoad());
-        $this->assertEquals(BigDecimal::of(0.2), $hosts[2]->getLoad());
+        foreach ($expectedLoads as $requestCount => $expectedLoad) {
+            $loadBalancer->handleRequest($request);
+            $this->assertEquals($expectedLoad[0], $hosts[0]->getLoad(), "Load for host 0 after request $requestCount");
+            $this->assertEquals($expectedLoad[1], $hosts[1]->getLoad(), "Load for host 1 after request $requestCount");
+            $this->assertEquals($expectedLoad[2], $hosts[2]->getLoad(), "Load for host 2 after request $requestCount");
+        }
     }
 
     /**
@@ -61,35 +95,62 @@ class LoadBalancerTest extends TestCase
         $request = new Request(BigDecimal::of('0.1'));
         $loadBalancer = new LoadBalancer($hosts, LoadBalancer::LOAD_BASED);
 
-        // First request should go to the first host (load < 0.75)
-        $loadBalancer->handleRequest($request);
-        $this->assertEquals(BigDecimal::of('0.6'), $hosts[0]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.8'), $hosts[1]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.71'), $hosts[2]->getLoad());
+        $expectedLoads = [
+            // After 1st request: Sent to Host 1 (load < 0.75)
+            1 => [
+                BigDecimal::of('0.6'), // Load for Host 1
+                BigDecimal::of('0.8'), // Load for Host 2
+                BigDecimal::of('0.71') // Load for Host 3
+            ],
+            // After 2nd request: Sent to Host 1 again (load < 0.75)
+            2 => [
+                BigDecimal::of('0.7'), // Load for Host 1
+                BigDecimal::of('0.8'), // Load for Host 2
+                BigDecimal::of('0.71') // Load for Host 3
+            ],
+            // After 3rd request: Sent to Host 1 again (load < 0.75)
+            3 => [
+                BigDecimal::of('0.8'), // Load for Host 1
+                BigDecimal::of('0.8'), // Load for Host 2
+                BigDecimal::of('0.71') // Load for Host 3
+            ],
+            // After 4th request: Sent to Host 3 (load < 0.75)
+            4 => [
+                BigDecimal::of('0.8'), // Load for Host 1
+                BigDecimal::of('0.8'), // Load for Host 2
+                BigDecimal::of('0.81') // Load for Host 3
+            ],
+            // After 5th request: Sent to Host 1 with the lowest load (all loads >= 0.75)
+            5 => [
+                BigDecimal::of('0.9'), // Load for Host 1
+                BigDecimal::of('0.8'), // Load for Host 2
+                BigDecimal::of('0.81') // Load for Host 3
+            ],
+            // After 6th request: Sent to Host 2 with the lowest load (all loads >= 0.75)
+            6 => [
+                BigDecimal::of('0.9'), // Load for Host 1
+                BigDecimal::of('0.9'), // Load for Host 2
+                BigDecimal::of('0.81') // Load for Host 3
+            ],
+            // After 7th request: Sent to Host 2 with the lowest load (all loads >= 0.75)
+            7 => [
+                BigDecimal::of('0.9'), // Load for Host 1
+                BigDecimal::of('0.9'), // Load for Host 2
+                BigDecimal::of('0.91') // Load for Host 3
+            ],
+        ];
 
-        // Second request should go to the second host (load < 0.75)
-        $loadBalancer->handleRequest($request);
-        $this->assertEquals(BigDecimal::of('0.7'), $hosts[0]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.8'), $hosts[1]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.71'), $hosts[2]->getLoad());
+        foreach ($expectedLoads as $requestNumber => $expectedLoad) {
+            $loadBalancer->handleRequest($request);
 
-        // Third request should go to the first host again (still load < 0.75)
-        $loadBalancer->handleRequest($request);
-        $this->assertEquals(BigDecimal::of('0.8'), $hosts[0]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.8'), $hosts[1]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.71'), $hosts[2]->getLoad());
-
-        // Fourth request should go to the third host (load < 0.75)
-        $loadBalancer->handleRequest($request);
-        $this->assertEquals(BigDecimal::of('0.8'), $hosts[0]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.8'), $hosts[1]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.81'), $hosts[2]->getLoad());
-
-        // Fifth request should go to the first host with the lowest load since all are >= 0.75
-        $loadBalancer->handleRequest($request);
-        $this->assertEquals(BigDecimal::of('0.9'), $hosts[0]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.8'), $hosts[1]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.81'), $hosts[2]->getLoad());
+            foreach ($hosts as $index => $host) {
+                $this->assertEquals(
+                    $expectedLoad[$index],
+                    $host->getLoad(),
+                    "Load mismatch after request #{$requestNumber} for host #{$index}"
+                );
+            }
+        }
     }
 
 
@@ -109,33 +170,50 @@ class LoadBalancerTest extends TestCase
         $request = new Request(BigDecimal::of('0.1'));
         $loadBalancer = new LoadBalancer($hosts, LoadBalancer::LOAD_BASED);
 
-        // First request should go to the fourth host with the lowest load since all are >= 0.75
-        $loadBalancer->handleRequest($request);
-        $this->assertEquals(BigDecimal::of('0.9'), $hosts[0]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.95'), $hosts[1]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.85'), $hosts[2]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.81'), $hosts[3]->getLoad());
+        $expectedLoads = [
+            // After 1st request: Sent to the fourth host with the lowest load (all loads >= 0.75)
+            1 => [
+                BigDecimal::of('0.9'), // Load for Host 1
+                BigDecimal::of('0.95'), // Load for Host 2
+                BigDecimal::of('0.85'), // Load for Host 3
+                BigDecimal::of('0.81')  // Load for Host 4 (lowest load)
+            ],
+            // After 2nd request: Sent to the fourth host with the lowest load (all loads >= 0.75)
+            2 => [
+                BigDecimal::of('0.9'), // Load for Host 1
+                BigDecimal::of('0.95'), // Load for Host 2
+                BigDecimal::of('0.85'), // Load for Host 3
+                BigDecimal::of('0.91')  // Load for Host 4 (updated after 1st request)
+            ],
+            // After 3rd request: Sent to the third host with the lowest load (all loads >= 0.75)
+            3 => [
+                BigDecimal::of('0.9'), // Load for Host 1
+                BigDecimal::of('0.95'), // Load for Host 2
+                BigDecimal::of('0.95'), // Load for Host 3 (updated after 2nd request)
+                BigDecimal::of('0.91')  // Load for Host 4
+            ],
+            // After 4th request: Sent to the third host with the lowest load (all loads >= 0.75)
+            4 => [
+                BigDecimal::of('1.0'), // Load for Host 1
+                BigDecimal::of('0.95'), // Load for Host 2
+                BigDecimal::of('0.95'), // Load for Host 3 (updated after 3rd request)
+                BigDecimal::of('0.91')  // Load for Host 4
+            ],
+        ];
 
-        // Second request should go to the fourth host with the lowest load since all are >= 0.75
-        $loadBalancer->handleRequest($request);
-        $this->assertEquals(BigDecimal::of('0.9'), $hosts[0]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.95'), $hosts[1]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.85'), $hosts[2]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.91'), $hosts[3]->getLoad());
+        foreach ($expectedLoads as $requestNumber => $expectedLoad) {
+            $loadBalancer->handleRequest($request);
 
-        // Third request should go to the third host with the lowest load since all are >= 0.75
-        $loadBalancer->handleRequest($request);
-        $this->assertEquals(BigDecimal::of('0.9'), $hosts[0]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.95'), $hosts[1]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.95'), $hosts[2]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.91'), $hosts[3]->getLoad());
+            foreach ($hosts as $index => $host) {
+                $this->assertEquals(
+                    $expectedLoad[$index],
+                    $host->getLoad(),
+                    "Load mismatch after request #{$requestNumber} for host #{$index}"
+                );
+            }
+        }
 
-        // Fourth request should go to the third host with the lowest load since all are >= 0.75
-        $loadBalancer->handleRequest($request);
-        $this->assertEquals(BigDecimal::of('1.0'), $hosts[0]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.95'), $hosts[1]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.95'), $hosts[2]->getLoad());
-        $this->assertEquals(BigDecimal::of('0.91'), $hosts[3]->getLoad());
+
     }
 
     public function testItHandlesRequestRoundRobin(): void
